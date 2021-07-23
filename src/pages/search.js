138cbe33c5e8d752ca8db7272c0823a08e2ffbcc
salem84 <giorgio.lasala@gmail.com>
2021-07-23 20:11:43 +0200

    search page

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,18 +1,71 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
 import Global from '../classy-docs/styles/global';
 import Layout from '../classy-docs/components/Layout';
+
+import Branding from '../classy-docs/components/Branding';
+import Footer from '../classy-docs/components/Footer';
+import Header from '../classy-docs/components/Header';
+import Menu from '../classy-docs/components/Menu';
+import Seo from '../classy-docs/components/Seo';
+
 import Search from "../components/search";
 
-const SearchPage = () => {
-  
+import config from '../../content/meta/config';
+import menuItems from '../../content/meta/menu';
+
+const SearchPage = props => {
+  const {
+    data: {
+      footerLinks: { html: footerLinksHTML },
+      copyright: { html: copyrightHTML },
+    },
+  } = props;
+
+  const {
+    headerTitle,
+    headerSubTitle,
+    siteUrl,
+    siteTitle,
+    siteDescription,
+    siteLanguage,
+  } = config;
+
+
     return (
       <Layout>
-        <Global />
-        <Search />
-      </Layout>
+      <Global />
+      <Header>
+        <Branding title={headerTitle} subTitle={headerSubTitle} />
+        <Menu items={menuItems} />
+      </Header>
+      <Search/>
+      <Footer links={footerLinksHTML} copyright={copyrightHTML} />
+      <Seo
+        url={siteUrl}
+        language={siteLanguage}
+        title={siteTitle}
+        description={siteDescription}
+      />
+    </Layout>
     );
   };
   
   export default SearchPage;
   
+  export const query = graphql`
+  query {
+
+    footerLinks: markdownRemark(
+      fileAbsolutePath: { regex: "/content/parts/footerLinks/" }
+    ) {
+      html
+    }
+    copyright: markdownRemark(
+      fileAbsolutePath: { regex: "/content/parts/copyright/" }
+    ) {
+      html
+    }
+  }
+`;
