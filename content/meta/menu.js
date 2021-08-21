bc4f5f3ce905d3430df55736ce5ad0fdb19ee76a
salem84 <giorgio.lasala@gmail.com>
2021-08-21 19:27:03 +0200

    tags page and refactoring

--- content/meta/menu.js
+++ content/meta/menu.js
@@ -4,7 +4,8 @@ import InfoIcon from 'react-feather/dist/icons/info';
 
 const menu = [
   { label: 'Home', to: '/', icon: HomeIcon },
-  { label: 'Pills', to: '/content', icon: ListIcon },
+  { label: 'By Category', to: '/content', icon: ListIcon },
+  { label: 'By Tag', to: '/content', icon: ListIcon },
   { label: 'About', to: '/about', icon: InfoIcon },
   // { label: 'Search', to: '/search', icon: InfoIcon }
 ];
